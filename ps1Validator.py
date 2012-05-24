52c96106296af1f73eb18852ca563cd4e763d449
Brian Ustas <bjjustas@gmail.com>
2012-05-24 18:07:16 +0200

    Changed some variable names and fixed output formatting

--- ps1Validator.py
+++ ps1Validator.py
@@ -2,7 +2,7 @@
 
 import re
 
-colorRE = re.compile(r'\\e\[\d{,2};?\d{,3}m')
+colorRegex = re.compile(r'\\e\[\d{,2};?\d{,3}m')
 
 # Prompt Variables from: 
 # http://www.gnu.org/software/bash/manual/html_node/Printing-a-Prompt.html 
@@ -38,6 +38,7 @@ def logIssue(ps1, pos, msg, err=True):
     print('{0}\n{1}^\n{2}: {3}'.format(ps1, '-' * (pos - 1),
                                        'Error' if err else 'Warning', msg))
     if err:
+        print('Exiting...')
         raise SystemExit(0)
     
 def validVar(testVar):
@@ -53,14 +54,14 @@ def validateVar(ps1):
     if match is not False:
         return len(match['match']) 
     else:
-        logIssue(ps1, 2, '{0} is an invalid prompt variable'.format(ps1[:2]))
+        logIssue(ps1, 2, '"{0}" is an invalid prompt variable.'.format(ps1[:2]))
 
 def validateColor(ps1):
-    colorStr = re.match(colorRE, ps1)
+    colorStr = re.match(colorRegex, ps1)
     if colorStr:
         return colorStr.group(0)
     else:
-        logIssue(ps1, 0, 'Color code is not properly formatted')
+        logIssue(ps1, 0, 'Color code is not properly formatted.')
 
 def validateEscape(ps1):
     pos = 0
@@ -75,7 +76,7 @@ def validateEscape(ps1):
                 return pos + 2
             else:
                 logIssue(ps1, pos,
-                         'Escape sequence not closed after declared color code')
+                         'Escape sequence not closed after declared color code.')
         else:
             pass
         
@@ -97,18 +98,21 @@ def parsePS1(ps1):
             match = validVar('\\' + ps1[pos])
             if match is not False:
                 logIssue(ps1, pos + 1,
-                         'Did you mean {0} ({1})?'.format(match['match'], match['type']), 
+                         'Did you mean "{0}" ({1})?'.format(match['match'], 
+                                                          match['type']), 
                          False)
             pos += 1
     
-    print('{0} is a valid PS1'.format(ps1))
+    print('Success: "{0}" is a valid PS1.'.format(ps1))
     
 def main():
     import os
-    
+    test = r'\u@\z: '
+    parsePS1(test)
+    """ 
     validPS1s = open("validPS1s.txt")
     for line in validPS1s:
         if line[0] != '#' and line[0] != '\n':
             parsePS1(line.strip())
-
+    """
 if __name__ == "__main__": main()
