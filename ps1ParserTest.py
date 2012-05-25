18c4fbf693fc4215a263cb0c7cc66c1eb962b06b
Brian Ustas <bjjustas@gmail.com>
2012-05-25 10:30:14 +0200

    The testing suite now properly tests the contents of testPS1s.txt

--- ps1ParserTest.py
+++ ps1ParserTest.py
@@ -2,12 +2,33 @@
 
 import ps1Parser
 
+tests = 0
+testsPassed = 0
 testPS1s = open('testPS1s.txt')
+
+def test(ps1, validPS1):
+    global tests, testsPassed 
+    
+    tests += 1
+    print('{0}.'.format(tests))
+    
+    ps1 = ps1.strip()
+    isValid = ps1Parser.parse(ps1[1:])
+
+    if isValid is validPS1:
+        testsPassed += 1
+        result = 'Pass'
+    else:
+        result = 'Fail'
+
+    print('{0}: This PS1 is{1}valid.\n'
+          .format(result, ' ' if validPS1 is True else ' NOT '))
 
 for ps1 in testPS1s:
     if ps1[0] == '+': 
-        isValid = ps1Parser.parse(ps1[1:].strip())
-        print('{0}: {1}'.format('Pass' if isValid is True else 'Fail', ps1))
+        test(ps1, True)
     elif ps1[0] == '-':
-        isValid = ps1Parser.parse(ps1[1:].strip())
-        print('{0}: {1}'.format('Pass' if isValid is False else 'Fail', ps1)) 
+        test(ps1, False)
+
+print('{0}\n{1} tests passed out of {2}.\n{0}'.format('-' * 40, 
+                                                      testsPassed, tests))
