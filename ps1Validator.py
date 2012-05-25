aa61cdce5f69e5fb240539e3c65202d1cb2adf62
Brian Ustas <bjjustas@gmail.com>
2012-05-25 02:05:24 +0200

    Made a singleton class called PS1Parser

--- ps1Validator.py
+++ ps1Validator.py
@@ -2,7 +2,7 @@
 
 import re
 
-colorRegex = re.compile(r'\\e\[\d{,2};?\d{,3}m')
+colorRegex = re.compile(r'\\(e|033)\[\d{,2};?\d{,3}m')
 
 # Prompt variables from: 
 # http://www.gnu.org/software/bash/manual/html_node/Printing-a-Prompt.html 
@@ -34,76 +34,87 @@ promptVars = {
     'strftime':                     r'\\D\{%[a-zA-z\+%]+\}'
 }
 
-def logIssue(ps1, pos, msg, err=True):
-    print('{0}: {1}\n{2}\n{3}^'.format('Error' if err else 'Warning', msg,
-                                         ps1, '-' * (pos - 1)))
-    if err:
-        print('Exiting...')
-        raise SystemExit(0)
+class PS1Parser():
+    _singletonInst = None
     
-def validVar(testVar):
-    for key in promptVars:
-        match = re.match(promptVars[key], testVar)
-        if match is not None:
-            return {'match': match.group(0), 'type': key}
+    def parse(self, ps1): 
+        self.ps1 = ps1
+        self._pos = 0
+        l = len(ps1)
 
-    return False
-
-def validateVar(ps1):
-    match = validVar(ps1)
-    if match is not False:
-        return len(match['match']) 
-    else:
-        logIssue(ps1, 2, '"{0}" is an invalid prompt variable.'.format(ps1[:2]))
-
-def validateColor(ps1):
-    colorStr = re.match(colorRegex, ps1)
-    if colorStr:
-        return colorStr.group(0)
-    else:
-        logIssue(ps1, 0, 'Color code is not properly formatted.')
-
-def validateEscape(ps1):
-    pos = 0
-    l = len(ps1)
-
-    while pos < l:
-        if ps1[pos:pos + 2] == r'\e':
-            colorStr = validateColor(ps1[pos:]) 
-            pos += len(colorStr)
-        if ps1[pos:pos + 2] == r'\]':
-            # Return the length of the escaped expression
-            return pos + 2
-
-        pos += 1
-    
-    logIssue(ps1, 0, 'Escape sequence was never closed.')
-
-def parsePS1(ps1):
-    pos = 0
-    l = len(ps1)
-
-    while pos < l:
-        if ps1[pos] == '\\':
-            if ps1[pos + 1] == r'[':
-                pos += validateEscape(ps1[pos:])
+        while self._pos < l:
+            if ps1[self._pos] == '\\':
+                if ps1[self._pos + 1] == r'[':
+                    self._pos += self.validateEscape(ps1[self._pos:])
+                else:
+                    self._pos += self.validateVar(ps1[self._pos:])        
             else:
-                pos += validateVar(ps1[pos:])        
+                match = self.validVar('\\' + ps1[self._pos])
+                if match is not False:
+                    self.logIssue(1,
+                             'Did you mean "{0}" ({1})?'.format(match['match'], 
+                                                                match['type']), 
+                             False)
+                self._pos += 1
+        
+        print('Success: "{0}" is a valid PS1.'.format(ps1))
+
+    def logIssue(self, deltaPos, msg, err=True):
+        # Minus 1 to leave room for caret
+        deltaPos += self._pos - 1
+
+        print('{0}: {1}\n{2}\n{3}^'.format('Error' if err else 'Warning', msg,
+                                           self.ps1, '-' * deltaPos))
+        if err:
+            print('Exiting...')
+            raise SystemExit(0)
+        
+    def validVar(self, testVar):
+        for key in promptVars:
+            match = re.match(promptVars[key], testVar)
+            if match is not None:
+                return {'match': match.group(0), 'type': key}
+
+        return False
+
+    def validateVar(self, ps1):
+        match = self.validVar(ps1)
+        if match is not False:
+            return len(match['match']) 
         else:
-            match = validVar('\\' + ps1[pos])
-            if match is not False:
-                logIssue(ps1, pos + 1,
-                         'Did you mean "{0}" ({1})?'.format(match['match'], 
-                                                            match['type']), 
-                         False)
+            self.logIssue(2, '"{0}" is an invalid prompt variable.'.format(ps1[:2]))
+
+    def validateColor(self, ps1):
+        colorStr = re.match(colorRegex, ps1)
+        if colorStr:
+            return colorStr.group(0)
+        else:
+            self.logIssue(0, 'Color code is not properly formatted.')
+
+    def validateEscape(self, ps1):
+        pos = 0
+        l = len(ps1)
+
+        while pos < l:
+            if ps1[pos:pos + 2] == r'\e':
+                colorStr = self.validateColor(ps1[pos:]) 
+                pos += len(colorStr)
+            if ps1[pos:pos + 2] == r'\]':
+                # Return the length of the escaped expression
+                return pos + 2
+
             pos += 1
-    
-    print('Success: "{0}" is a valid PS1.'.format(ps1))
-    
+        
+        self.logIssue(0, 'Escape sequence was never closed.')
+        
+
 def main():
     import os
-    test = r'\[\e[0;30mryuh\]tj: '
-    parsePS1(test)
+
+    parser = PS1Parser()
+    test = r'\[\e[0;30mryuhx\]\ztj: '
+    parser.parse(test)
+
     """ 
     validPS1s = open("validPS1s.txt")
     for line in validPS1s:
