21eef3f41f1561626f63bcc25e29e3a6528bde72
Brian Ustas <bjjustas@gmail.com>
2012-06-11 14:23:15 +0200

    Made global variables static.

--- tests/ps1LintTest.py
+++ tests/ps1LintTest.py
@@ -1,37 +1,34 @@
 import ps1Lint
 
-lineNum = 0
-tests = 0
-testsPassed = 0
-testPS1s = open('tests/ps1s.txt')
+def test():
+    lineNum = 0
+    test.tests = 0
+    test.testsPassed = 0
 
-def test(lineNum, ps1, validPS1):
-    global tests, testsPassed 
-    
-    tests += 1
-    print('{0}. Line {1}'.format(tests, lineNum))
+    with open('tests/ps1s.txt', 'r') as testPS1s:
+        for ps1 in testPS1s:
+            lineNum += 1
+            
+            if ps1[0] == '+': 
+                testLine(lineNum, ps1, True)
+            elif ps1[0] == '-':
+                testLine(lineNum, ps1, False)
+
+    print('{0}\n{1} tests passed out of ''{2}.\n'
+          '{0}'.format('-' * 40, test.testsPassed, test.tests))
+
+def testLine(lineNum, ps1, validPS1):
+    test.tests += 1
+    print('{0}. Line {1}'.format(test.tests, lineNum))
     
     ps1 = ps1.strip()
     isValid = ps1Lint.parse(ps1[1:])
 
     if isValid is validPS1:
-        testsPassed += 1
+        test.testsPassed += 1
         result = 'Pass'
     else:
         result = 'Fail'
 
     print('{0}: This PS1 is{1}valid.\n'
           .format(result, ' ' if validPS1 is True else ' NOT '))
-
-for ps1 in testPS1s:
-    lineNum += 1
-    
-    if ps1[0] == '+': 
-        test(lineNum, ps1, True)
-    elif ps1[0] == '-':
-        test(lineNum, ps1, False)
-
-testPS1s.close()
-
-print('{0}\n{1} tests passed out of {2}.\n{0}'.format('-' * 40, 
-                                                      testsPassed, tests))
