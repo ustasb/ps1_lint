1767722fcf5c1ccebcce5def65f2244a6e919503
Brian Ustas <bjjustas@gmail.com>
2012-06-12 22:48:31 +0200

    Updated version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'PS1Lint',
-    version = '0.1',
+    version = '0.2',
     py_modules = ['ps1Lint'],
     license  =  'MIT License',
     long_description = open('README.md').read(),
