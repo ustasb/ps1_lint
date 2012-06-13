d98f09d0c44b78a921bbdff6cb3a3245eca33fbf
Brian Ustas <bjjustas@gmail.com>
2012-06-13 23:22:39 +0200

    Improved cursor movement support.

--- ps1Lint.py
+++ ps1Lint.py
@@ -31,11 +31,11 @@ PROMPT_VARS = (
 )
 
 # Anything inside ``, \$(), ${} or formatted as $<varName> is ignored.
-SHELL_CODE_REGEX = re.compile(r'(`[^`]+`|\\\$\([^)]+\)|\$\{[^\}]+\}|\$\w+)+')
+CMD_CODE_REGEX = re.compile(r'(`[^`]+`|\\\$\([^)]+\)|\$\{[^\}]+\}|\$\w+)+')
 ESC_SEQ_START_REGEX = re.compile(r'\\(e|033)\[')
 # Control Sequence Introducers (CSI)
 COLOR_CSI_REGEX = re.compile(r'(([0-8]|3[0-7]|4[0-7]);){0,2}([0-8]|3[0-7]|4[0-7])m')
-CURSOR_MVMENT_CSI_REGEX = re.compile(r'(2J|\d.*[ABCD]|\d*;\d*[Hf]|[suK])(?!.*m)')
+CURSOR_MVMENT_CSI_REGEX = re.compile(r'^(2J|[suK]|(\d+([ABCD]|;\d+[Hf]))|(\d+;)+\d+m)')
 
 # Custom exception class
 class PS1Error(SyntaxError):
@@ -52,7 +52,7 @@ def parse(ps1):
 
             # If `, \$(, or ${ is encountered, look for its closing equivalent.
             if re.match(r'`|\\\$\(|\$\{', ps1[parserPos:]):
-                commandSeq = re.match(SHELL_CODE_REGEX, ps1[parserPos:])
+                commandSeq = re.match(CMD_CODE_REGEX, ps1[parserPos:])
                 if commandSeq is not None:
                     parserPos += len(commandSeq.group(0))
                 else:
@@ -60,8 +60,9 @@ def parse(ps1):
                 continue
             
             elif re.match(ESC_SEQ_START_REGEX, ps1[parserPos:]) is not None:
-                raise PS1Error(0, 'Color or cursor sequence encountered '
-                                  'without being escaped by \[ ... \].')
+                raise PS1Error(0, 'Color or cursor movement sequence '
+                                  'encountered without being escaped by '
+                                  '\[ ... \].')
 
             # Don't match backslashes followed by white space or at the end
             # of the line.
@@ -91,49 +92,52 @@ def validateVar(ps1):
             return len(match.group(0))
 
     if re.match(r'a|r', ps1):
-        raise PS1Error(0, '"\\{0}" causes line wrapping issues and shouldn\'t '
+        raise PS1Error(-1, '"\\{0}" causes line wrapping issues and should not '
                           'be used.'.format(ps1[0]))
     elif re.match(r'\d{3}', ps1):
-        raise PS1Error(0, '"\\{0}" is an invalid ASCII code. ASCII octal codes '
-                          'must be between 040 and 176.'.format(ps1[:4]))
+        raise PS1Error(-1, '"\\{0}" is an invalid ASCII octal code--it must '
+                          'be between 040 and 176.'.format(ps1[:4]))
     else: 
-        raise PS1Error(0, '"{0}" is an invalid prompt variable.'.format(ps1[0]))
+        raise PS1Error(-1, '"\\{0}" is an invalid prompt variable.'.format(ps1[0]))
 
 def validateNonPrintSeq(ps1):
     pos = 0
     ps1Len = len(ps1)
     
     while pos < ps1Len:
-        escSeqBegin = re.match(ESC_SEQ_START_REGEX, ps1[pos:pos + 5]) 
-        if escSeqBegin is not None:
-            pos += len(escSeqBegin.group(0))
+        escSeqStart = re.match(ESC_SEQ_START_REGEX, ps1[pos:pos + 5]) 
+        if escSeqStart is not None:
+            escSeqStartLen = len(escSeqStart.group(0)) 
+            pos += escSeqStartLen
 
             colorCSI = re.match(COLOR_CSI_REGEX, ps1[pos:])
             if colorCSI is not None:
+                seqType = 'color'
                 pos += len(colorCSI.group(0))
-
-                if ps1[pos:pos + 2] != r'\]':
-                    raise PS1Error(pos, 'Expecting escape sequence to close '
-                                        'after declared color code but it '
-                                        'didn\'t.')
             else:
                 cursorMvmentCSI = re.match(CURSOR_MVMENT_CSI_REGEX, ps1[pos:])
                 if cursorMvmentCSI is not None:
+                    seqType = 'cursor movement'
                     pos += len(cursorMvmentCSI.group(0))
                     print('Warning: Cursor movement sequences can cause '
                           'line wrapping issues.')
                 else:
-                    raise PS1Error(pos, 'Invalid color or cursor movement '
-                                        'sequence.')
+                    raise PS1Error(pos - escSeqStartLen, 'Invalid color or '
+                                                         'cursor movement '
+                                                         'sequence.')
 
+            if ps1[pos:pos + 2] != r'\]':
+                raise PS1Error(pos, 'Expecting non-printing sequence to '
+                                    'close after declared {0} sequence '
+                                    'but it did not.'.format(seqType))
         else:
-            match = re.match(SHELL_CODE_REGEX, ps1[pos:])
+            match = re.match(CMD_CODE_REGEX, ps1[pos:])
             if match is not None:
                 pos += len(match.group(0))
             else:
-                raise PS1Error(pos, '"{0}" shouldn\'t be here. Only color or '
-                                    'movement sequences should be put inside '
-                                    ' \[ ... \].'.format(ps1[pos])) 
+                raise PS1Error(pos, '"{0}" should not be here. Only color or '
+                                    'cursor movement sequences should be put '
+                                    'inside \[ ... \].'.format(ps1[pos])) 
 
         if ps1[pos:pos + 2] == r'\]':
             # Return the entire length of the escaped expression.
