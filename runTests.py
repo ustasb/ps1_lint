7ffb248d972043d827b696e31a1cbd6896664270
Brian Ustas <bjjustas@gmail.com>
2012-06-13 12:06:11 +0200

    Created a TESTS constant.

--- runTests.py
+++ runTests.py
@@ -5,12 +5,14 @@ import sys
 import timeit
 import tests.ps1LintTest
 
+TESTS = 50
+
 # Suppress output while timing tests.
 oldStdout = sys.stdout
 sys.stdout = open(os.devnull, 'w')
-timeTaken = timeit.timeit(tests.ps1LintTest.test, number=50)
+timeTaken = timeit.timeit(tests.ps1LintTest.test, number=TESTS)
 
 # Print a test to the console.
 sys.stdout = oldStdout
 tests.ps1LintTest.test()
-print('Running 50 times took {0} seconds.'.format(timeTaken))
+print('Running {0} times took {1} seconds.'.format(TESTS, timeTaken))
